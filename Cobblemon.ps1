73f79c383c8c039109f3ce9858f534c6cbacd560
Fabien <fabien.dussaux@gmail.com>
2025-03-08 03:20:19 +0100

    Update Cobblemon.ps1

--- Cobblemon.ps1
+++ Cobblemon.ps1
@@ -72,6 +72,15 @@ if (Test-Path $OutputPathIcons) {
     exit 1
 }
 
+# Suppression de l'ancienne version de CobblemonIcons, si elle existe
+$ExistingIconsFolder = Get-ChildItem -Path $InstancePath\resourcepacks -Directory | Where-Object { $_.Name -match "CobblemonIcons" }
+
+if ($ExistingIconsFolder) {
+    Write-Host "Suppression de l'ancienne version de CobblemonIcons..."
+    Remove-Item -Path $ExistingIconsFolder.FullName -Recurse -Force
+    Write-Host "Ancienne version de CobblemonIcons supprimee."
+}
+
 # Extraction des fichiers
 $DestinationCobbFR = "$InstancePath\resourcepacks\CobblemonFR"
 $DestinationIcons = "$InstancePath\resourcepacks"
@@ -93,38 +102,6 @@ if (Test-Path $DestinationIcons) {
     Write-Host "Erreur : Echec de l'extraction de CobblemonIcons."
     exit 1
 }
-
-# Trouver le nom du dossier CobblemonIcons extrait
-$ExtractedIconsFolders = Get-ChildItem -Path $DestinationIcons -Directory | Where-Object { $_.Name -match "CobblemonIcons" }
-
-if ($ExtractedIconsFolders.Count -eq 0) {
-    Write-Host "Erreur : Impossible de trouver le dossier CobblemonIcons."
-    exit 1
-} else {
-    $NewIconsFolder = $ExtractedIconsFolders[0].Name
-    Write-Host "Nouveau dossier detecte : $NewIconsFolder"
-}
-
-# Construire la ligne de resourcePacks avec le dossier correct
-$ResourcePacksLine = 'resourcePacks:["vanilla","fabric","cobbreeding:pasturefix","convenientdecor:hydrated_farmland","file/Boenk\u0027s_Cobblemon_Item_Description_V1.5.0.zip","file/Theone\u0027s Items Pack v0.8.zip","file/Cobblemon Interface v1.1.1.zip","file/Journeymap-Cobblemon3.01_1.20.1_MythicalNetwork.zip","file/EmbellishedStone-1.20.1-1.0.0.zip","file/VidyaPokeCriesCobblemon-V1.2.zip","file/EclecticTrove-1.20.1-1.3.0","file/Border-Makeover-Style-9","file/SmoothFont_1.20.zip","file/bopack","file/' + $NewIconsFolder + '","file/CobblemonFR","file/CobbleCafe","file/Vivillon Pride Patterns v1.0","file/BattleBond [V1.4]","file/Star Academy","Moonlight Mods Dynamic Assets"]'
-
-# Modifier le fichier options.txt
-$OptionsFilePath = "$InstancePath\options.txt"
-
-if (-Not (Test-Path $OptionsFilePath)) {
-    Write-Host "Erreur : Le fichier options.txt est introuvable."
-    exit 1
-}
-
-# Lire le contenu du fichier
-$OptionsContent = Get-Content $OptionsFilePath -Raw
-
-# Remplacer la ligne de resourcePacks avec la nouvelle version
-$UpdatedContent = $OptionsContent -replace 'resourcePacks:\[.*\]', $ResourcePacksLine
-
-# Écrire les modifications dans options.txt
-Set-Content -Path $OptionsFilePath -Value $UpdatedContent
-Write-Host "Le fichier options.txt a ete mis a jour."
 
 # Suppression de 7-Zip si installe par le script
 if ($InstalledByScript) {
